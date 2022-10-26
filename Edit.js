f5f8135384e6bc75470f1f79fd7b6e78a5781d8d
Mohd Ali <100376235+frissky@users.noreply.github.com>
2022-10-26 23:44:37 +0530

    Update Edit.js

--- Edit.js
+++ Edit.js
@@ -2,7 +2,7 @@ function name1(){
     let name = prompt('Player1');
     document.querySelector(".play1").innerHTML = name;
 }
-
+//with love from frissky
 function name2(){
     let name = prompt('Player2');
     document.querySelector(".play2").innerHTML = name;
